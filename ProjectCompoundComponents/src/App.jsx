5b5fd3be0963b01b2311f0132dc8ac13e5a544b3
Rafael Dametto <cacotpds@gmail.com>
2023-08-07 15:33:48 -0300

    Compoud Components  e Context API

--- ProjectCompoundComponents/src/App.jsx
+++ ProjectCompoundComponents/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Compound Components
+import { createContext, useContext, useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+const s = {
+  style: {
+    fontSize: '50px',
+  },
+};
+
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return (
+    <TurnOnOffContext.Provider value={{ isOn, onTurn }}>
+      {children}
+    </TurnOnOffContext.Provider>
+  );
+};
+const P = ({ children }) => <p {...s}>{children}</p>;
+
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+  return (
+    <button onClick={onTurn} {...props} className="onOff">
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React - Project Compound Components</h1>
+      <h3>React.Children + React.CloneElement</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}p
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div>
+          <TurnOnOff>
+            <div>
+              <p {...s}>oi</p>
+              <TurnedOn>
+                <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+              </TurnedOn>
+              <TurnedOff>
+                <P> Aqui as coisas do OFF.</P>
+              </TurnedOff>
+              <TurnButton {...s} />
+            </div>
+          </TurnOnOff>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
